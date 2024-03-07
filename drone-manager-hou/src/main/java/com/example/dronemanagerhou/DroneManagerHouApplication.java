360e7ecfa0f2124f0a3bc1e17672904826e52be1
sdfgfdg <1809431516@qq.com>
2024-03-07 12:42:53 +0800

    初始化仓库，添加代码文件

---
+++ drone-manager-hou/src/main/java/com/example/dronemanagerhou/DroneManagerHouApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronemanagerhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagerHouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneManagerHouApplication.class, args);
+    }
+
+}
---
+++ drone-manager-hou/src/test/java/com/example/dronemanagerhou/DroneManagerHouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronemanagerhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagerHouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
