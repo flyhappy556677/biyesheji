d27057e6fd55560f55fe919c82bfe049c0ca05a7
sdfgfdg <1809431516@qq.com>
2024-03-11 20:49:45 +0800

    merge

--- drone-manager-hou/src/main/java/com/example/dronemanagerhou/DroneManagerHouApplication.java
+++ drone-manager-hou/src/main/java/com/example/dronemanagerhou/DroneManagerHouApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DroneManagerHouApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DroneManagerHouApplication.class, args);
     }
